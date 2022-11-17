e2b279cb2e5f1f63d69d8469603745605e7f8895
Luis Esteban <107282178+bitazaratustra@users.noreply.github.com>
2022-11-17 00:31:23 -0300

    Decision science
    Sim. Work with real data from olist (without secrets dates). Data analysis, data exploration and decision make

---
+++ 04-Decision-Science/olist/data.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+
+
+class Olist:
+    def get_data(self):
+        """
+        This function returns a Python dict.
+        Its keys should be 'sellers', 'orders', 'order_items' etc...
+        Its values should be pandas.DataFrames loaded from csv files
+        """
+        # Hints 1: Build csv_path as "absolute path" in order to call this method from anywhere.
+            # Do not hardcode your path as it only works on your machine ('Users/username/code...')
+            # Use __file__ instead as an absolute path anchor independant of your usename
+            # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
+        # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
+        csv_path = os.path.join(os.path.dirname(__file__),'../data/csv')
+        file_names = os.listdir(csv_path)
+        file_names.remove('.gitkeep')
+        names_csv = [names.replace(".csv", "") for names in file_names]
+        names_dataset = [names.replace("_dataset", "") for names in names_csv]
+        key_names = [names.replace("olist_", "") for names in names_dataset]
+        data = {x: pd.read_csv(os.path.join(csv_path, y)) for (x,y) in zip(key_names, file_names)}
+        return data
+
+
+    def ping(self):
+        """
+        You call ping I print pong.
+        """
+        print("pong")
