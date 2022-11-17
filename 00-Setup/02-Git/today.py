8869d153df5b024cedfd94ec9406876303fdb478
Luis Esteban <107282178+bitazaratustra@users.noreply.github.com>
2022-11-17 00:11:41 -0300

    First day - Setup challenges

---
+++ 00-Setup/02-Git/today.py
@@ -0,0 +1,10 @@
+'''
+This is the module docstring
+'''
+def my_name_is():
+    '''my name '''
+    return ' bitazaratustra'
+
+def my_buddy_is():
+    """A module computing buddy pair names for the day"""
+    return ' Adrian Hernandez Aguilera'
