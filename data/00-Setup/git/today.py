2de30becafa11dbf06839d9bbcc0b5517d31a720
Luis Esteban <ebaldasseroni@gmail.com>
2023-02-16 17:05:16 -0300

    Fix style issues, should get a 'Good Style' now 🙏

--- data/00-Setup/git/today.py
+++ data/00-Setup/git/today.py
@@ -1,10 +1,16 @@
-'''
-This is the module docstring
-'''
+"""A module computing buddy pair names for the day"""
 
 def my_name_is():
+
+    """A module computing buddy pair names for the day"""
     return str('bitazaratustra')
 
 
-def my_buddy_name():
+
+
+
+def my_buddy_is():
+
+    """A module computing buddy pair names for the day"""
+
     return str('bitazaratustra')
