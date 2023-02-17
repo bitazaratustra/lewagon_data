d34f797419829ff4a2339c763502bdaa67ca1b5a
Luis Esteban <ebaldasseroni@gmail.com>
2023-02-17 13:11:48 -0300

    Currency challenge

---
+++ data-data-structures/currencies.py
@@ -0,0 +1,19 @@
+# pylint: disable=missing-docstring
+
+# TODO: add some currency rates
+RATES = {'USDEUR': 0.85,
+ 'GBPEUR': 1.13,
+ 'CHFEUR': 0.86,
+ 'EURGBP': 0.885
+}
+
+def convert(amount, currency):
+    """returns the converted amount in the given currency
+    amount is a tuple like (100, "EUR")
+    currency is a string
+    """
+    for key, value in RATES.items():
+        if amount[1] == key[:3] and currency == 'EUR':
+            return round(value * amount[0])
+        if amount[1] == key[3:6] and currency == key[:3]:
+            return round(amount[0] / value)
