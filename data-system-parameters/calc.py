91366aad4eee136323d3f0c8f24d829851bd68c9
Luis Esteban <ebaldasseroni@gmail.com>
2023-02-20 12:22:38 -0300

    calaculator as script with sys.argv[]

---
+++ data-system-parameters/calc.py
@@ -0,0 +1,19 @@
+# pylint: disable=missing-module-docstring,missing-function-docstring,eval-used
+import sys
+
+
+def main():
+    """Implement the calculator"""
+    if sys.argv[2] == '+':
+        return int(sys.argv[1]) + int(sys.argv[3])
+    if sys.argv[2] == '-':
+        return int(sys.argv[1]) - int(sys.argv[3])
+    if sys.argv[2] == '/':
+        return int(sys.argv[1]) / int(sys.argv[3])
+    if sys.argv[2] == '*':
+        return int(sys.argv[1]) * int(sys.argv[3])
+    return 'error'
+
+
+if __name__ == "__main__":
+    print(main())
