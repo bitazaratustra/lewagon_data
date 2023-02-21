cb589dddb77c91775fac27b2d31aeb360d317408
Luis Esteban <ebaldasseroni@gmail.com>
2023-02-21 18:32:30 -0300

    sys.argv{}

--- data-debugging/hello.py
+++ data-debugging/hello.py
@@ -5,11 +5,8 @@ import sys
 def full_name(first_name, last_name):
     """returns the full name"""
     name = f"{first_name.capitalize()}{last_name.capitalize()}"
-    # $DELETE_BEGIN
-    # Uncomment the line below and run your code to see the debugger in action
     # breakpoint()
     name = f"{first_name.capitalize()} {last_name.capitalize()}".strip()
-    # $DELETE_END
     return name
 
 if __name__ == "__main__":
